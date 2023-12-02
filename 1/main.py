7c1ab5491d5a0589a85ca99f13bab6dcaccf03d2
huaraina <95729626+huaraina@users.noreply.github.com>
2023-12-01 22:38:16 -0500

    Update main.py

--- 1/main.py
+++ 1/main.py
@@ -1,12 +1,18 @@
 import re
 
-list = open("index.txt")
-y=0
-i=0
-while i<999:
-  x = list.readlines()[i]
-  re.sub("[^0-9]", "", f"{x}")
-  y = y+x
-  i = i+1
-print(y)
-  
+total = 0 #setup number variable
+words = 'one two three four five six seven eight nine'.split()
+
+reFunc = '(?=(' + '|'.join(words) + '|\\d))' #regex to get words
+
+def f(nums):
+    if nums in words:
+        return str(words.index(nums)+1)
+    return nums 
+
+for nums in open("index.txt"):
+    needed = [*map(f, re.findall(reFunc, nums))] #check through the characters in the thingy
+    total += int(needed[0]+needed[-1]) #add first and last by rolling the last part over
+print(total) #print total
+
+#i kinda lost track of what i was doing like halfway through and forgot to notate it and forgot some of what it did
